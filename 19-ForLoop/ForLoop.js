e2a12a185cf5f85dc99e37f480079f810d5576f8
Anjali Sewmini <anjalisewmini5@gmail.com>
2025-07-20 13:25:54 +0530

    can learn about for loop

--- 19-ForLoop/ForLoop.js
+++ 19-ForLoop/ForLoop.js
@@ -1,2 +1,29 @@
 //////////////////////////////////For loop/////////////////////////////
 //for loop repeat some code a limited amount of times
+let i;
+for(i = 0; i < 10; i+=2 ){
+    console.log("i : " + i);
+}
+console.log("-----------------");
+let y;
+for(y=10; y>=0; y--){
+    console.log("y : " + y);
+}
+console.log("-----------------");
+let a;
+for(a = 0; a <= 10; a++ ){
+    if(a==5){
+        continue;  // skip 5 output and continue
+    }else{
+        console.log("a : " + a);
+    }
+}
+console.log("-----------------");
+let b;
+for(b = 0; b <= 10; b++ ){
+    if(b==5){
+        break;  // it brak in 5
+    }else{
+        console.log("b : " + b);
+    }
+}
